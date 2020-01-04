91bff3623f9646c2ae8d94bde634bb57abecdff2
K e n V <kenneth_valdivia@yahoo.com>
2020-01-04 01:13:30 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,29 @@
-# Add your code here
+def say_hello(name)
+  "Hi #{name}!"
+end
+ 
+puts "Enter your name:"
+users_name = gets.strip
+ 
+puts say_hello(users_name)
+
+I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
+
+list = [1. Phoenix - 1901
+2. Tokyo Police Club - Wait Up
+3. Sufjan Stevens - Too Much
+4. The Naked and the Famous - Young Blood
+5. (Far From) Home - Tiga
+6. The Cults - Abducted
+7. Phoenix - Consolation Prizes
+8. Harry Chapin - Cats in the Cradle
+9. Amos Lee - Keep It Loose, Keep It Tight]
+
+"Please enter a song name or number:" gets.strip
+
+puts "Goodbye"
+
