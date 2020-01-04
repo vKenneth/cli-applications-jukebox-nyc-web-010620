f6bd4cb73d1be0f76b2f002beae005fdc8623b63
K e n V <kenneth_valdivia@yahoo.com>
2020-01-04 01:04:22 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -7,3 +7,9 @@ users_name = gets.strip
  
 puts say_hello(users_name)
 
+I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
+
