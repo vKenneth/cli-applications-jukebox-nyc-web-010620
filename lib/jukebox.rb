9aeb75b87d509f8b7db6b498af4e611ea1aaf364
K e n V <kenneth_valdivia@yahoo.com>
2020-01-04 01:05:13 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -13,3 +13,13 @@ I accept the following commands:
 - play : lets you choose a song to play
 - exit : exits this program
 
+list = [1. Phoenix - 1901
+2. Tokyo Police Club - Wait Up
+3. Sufjan Stevens - Too Much
+4. The Naked and the Famous - Young Blood
+5. (Far From) Home - Tiga
+6. The Cults - Abducted
+7. Phoenix - Consolation Prizes
+8. Harry Chapin - Cats in the Cradle
+9. Amos Lee - Keep It Loose, Keep It Tight]
+
